850d741f1d5118085ec9b658c8c0496142d98995
Tirthak <patelti@eofe5.eth.cluster>
2018-04-13 12:45:06 -0400

    Adding runs

--- cppExecution/run10k.sh
+++ cppExecution/run10k.sh
@@ -2,11 +2,10 @@
 
 NUM_NODES=10000
 INIT_DEG=5
-HEAL_THRES=20
-DEAD_THRES=50
+HEAL_THRES=40
+DEAD_THRES=65
 ALPHA=0
 VERBOSE=0
-RUN_NUM=1
 
 while true
 do
@@ -14,7 +13,7 @@ do
 	while [ $ALPHA -le 10 ]
 	do
 		alpha=$(echo "scale=2;$ALPHA/10" | bc -l)
-		srun -N 1 -n 1 -p sched_mit_hill -t 00:05:00 ./resilience $NUM_NODES $INIT_DEG $HEAL_THRES $DEAD_THRES $alpha $VERBOSE >> out10k.dat
+		srun -N 1 -n 1 -p sched_mit_hill -t 00:15:00 ./resilience $NUM_NODES $INIT_DEG $HEAL_THRES $DEAD_THRES $alpha $VERBOSE >> out10k.dat
 		((ALPHA++))
 	done
 done
---
+++ cppExecution/run20k.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUM_NODES=20000
+INIT_DEG=5
+HEAL_THRES=40
+DEAD_THRES=65
+ALPHA=8
+VERBOSE=0
+
+while true
+do
+	alpha=$(echo "scale=2;$ALPHA/10" | bc -l)
+	srun -N 1 -n 1 --exclusive -p sched_mit_hill -t 01:00:00 ./resilience $NUM_NODES $INIT_DEG $HEAL_THRES $DEAD_THRES $alpha $VERBOSE >> out20k.dat
+done
